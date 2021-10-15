1a4b7fed5d7d985f56425a089deeeee453afe7ad
Mikipswso <mickey4649soccer@gmail.com>
2021-10-15 13:58:14 -0400

    added attributes and constructor to FoodItem

--- NutritionApp/src/FoodItem.java
+++ NutritionApp/src/FoodItem.java
@@ -1,2 +1,12 @@
-public class FoodItem {
+public class FoodItem implements Nutrient{
+    String name;
+    String type;
+    int amount; /* amount of food in grams */
+
+    public FoodItem(String name, String type, int amount) {
+        this.name = name;
+        this.type = type;
+        this.amount = amount;
+    }
+
 }
--- NutritionApp/src/Nutrient.java
+++ NutritionApp/src/Nutrient.java
@@ -1,2 +1,3 @@
-public class Nutrient {
+interface Nutrient {
+
 }
