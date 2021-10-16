6b79b83a331eee4c723df64a0bbd89b44952c38a
willassad <will.assad@mail.utoronto.ca>
2021-10-16 02:13:47 -0400

    Instructions for TODOS

--- NutritionApp/test/LoginSystemTest.java
+++ NutritionApp/test/LoginSystemTest.java
@@ -16,12 +16,23 @@ public class LoginSystemTest {
 
     @Test(timeout = 50)
     public void testUserExists() {
-        // TODO: write this test
+        // TODO: write this test.
+        // Create a new user using system.registerUser(username, password)
+        // Check that user exists with assertEquals(actual, expected)
+    }
+
+    @Test(timeout = 50)
+    public void testUserDoesNotExist() {
+        // TODO: write this test.
+        // Create a new user using system.registerUser(username, password)
+        // Check that a different username does not exist with assertEquals(actual, expected)
     }
 
     @Test(timeout = 50)
     public void testGetUser() {
         // TODO: write this test
+        // Create a new user using system.registerUser(username, password)
+        // Verify that the return value of system.getUser(username) is a User with username username.
     }
 
 }
