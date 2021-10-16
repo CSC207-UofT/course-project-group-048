dcd8a62208abcd2a639720172973d55170ce3918
willassad <will.assad@mail.utoronto.ca>
2021-10-16 02:08:32 -0400

    Added CRC cards, unit tests, and Phase 0 files

---
+++ NutritionApp/test/LoginSystemTest.java
@@ -0,0 +1,27 @@
+/*
+ * This file contains JUnit test cases for LoginSystem.java
+ */
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class LoginSystemTest {
+    LoginSystem system;
+
+    @Before
+    public void setUp() {
+        system = new LoginSystem();
+    }
+
+    @Test(timeout = 50)
+    public void testUserExists() {
+        // TODO: write this test
+    }
+
+    @Test(timeout = 50)
+    public void testGetUser() {
+        // TODO: write this test
+    }
+
+}
