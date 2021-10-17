62a736a09f934a840a1a08d9834bc3a79762ed09
willassad <willassad@icloud.com>
2021-10-16 23:36:30 -0400

    Create gradle project

--- NutritionApp/src/FoodItems.java
+++
@@ -1,9 +0,0 @@
-/*
-    A database of food items.
- */
-
-public class FoodItems {
-    final static FoodItem ORANGE = new FoodItem("orange", "fruit", 50);
-    final static FoodItem APPLE = new FoodItem("apple", "fruit", 41);
-    // TODO: add more food items
-}
