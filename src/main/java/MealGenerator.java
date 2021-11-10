b0815b81b69b457152fec4adbd289c4a8f28008d
harshshah <harsh108shah@gmail.com>
2021-11-10 01:56:07 -0500

    added comments

--- src/main/java/MealGenerator.java
+++ src/main/java/MealGenerator.java
@@ -1,3 +1,46 @@
 public class MealGenerator {
 
+    String mealType;
+    int caloriesAim;
+    String dietInfo;
+
+    /**
+     * A goal set for a given User.
+     *
+     * @param mealType the type of meal to be generated
+     * @param caloriesAim the target calories to be gained from the meal generated
+     * @param dietInfo dietary requirements (vegan, vegetarian, etc)
+     */
+
+    public void MealGenerator(String mt, int ca, String di) {
+        this.mealType = mt;
+        this.caloriesAim = ca;
+        this.dietInfo = di;
+    }
+
+    /*
+    private void customMeal(FoodItems fd) {
+        boolean possible = false
+        for(int i = 0; i < len(fd); i++) {
+             add conditions: enough items for a custom meal/*
+            if conditions meet, change possible to true
+
+        }
+        if (possible) {
+             since a meal is possible, generate a random one, and return it
+
+        }
+        else {
+             return not possible, use API instead
+        }
+    }
+
+    */
+
+    private void apiGenerator() {
+
+
+    }
 }
+
+
