419e29172bd593d1ee9fc60fc244c961fd5e941d
Mikipswso <mickey4649soccer@gmail.com>
2021-11-11 15:06:49 -0500

    created paula's branch and edited fooditems and fooditem. added foodClassification interface

--- src/main/java/FoodItem.java
+++ src/main/java/FoodItem.java
@@ -1,23 +1,32 @@
 /*
    FoodItem stores food name, type (fruit, vegetable, meat etc.)
-   and its amount in grams or units.
+   and its amount in grams and its corresponding nutritional values(carbohydrates, protein and fat)
  */
 
-public class FoodItem extends Nutrient {
+import java.util.HashMap;
+
+public class FoodItem{
     String name;
     String type;
     int amount;
+    String classification;
+    HashMap<String, Double> nutrients;
 
 
     /**
      * @param name name of the food
      * @param type type of the food(fruit, veg, meat etc.)
      * @param amount amount of the food
+     * @param classification classification of the FoodItem(breakfast, lunch, dinner and snacks)
+
      */
-    public FoodItem(String name, String type, int amount) {
+    public FoodItem(String name, String type, int amount, String classification) {
         this.name = name;
         this.type = type;
         this.amount = amount;
+        this.classification = classification;
+
+
 
     }
     public String getName() {
@@ -29,6 +38,10 @@ public class FoodItem extends Nutrient {
     public int getAmount() {
         return amount;
     }
+    public String getClassification() {return classification;}
 
 
-}
+    public void changeclassf(String newclassf){
+        this.classification = newclassf;
+    }
+    }
