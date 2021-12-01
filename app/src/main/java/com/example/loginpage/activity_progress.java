1820003fb62dbc2d0c28edbd62d6b08e59286e56
Mikipswso <mickey4649soccer@gmail.com>
2021-12-01 16:03:30 -0500

    added buttons & title to the progress page

--- app/src/main/java/com/example/loginpage/activity_progress.java
+++ app/src/main/java/com/example/loginpage/activity_progress.java
@@ -3,6 +3,12 @@ package com.example.loginpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class activity_progress extends AppCompatActivity {
 
