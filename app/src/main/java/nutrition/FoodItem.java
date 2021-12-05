cdc4dee58540536426ccde87d1a558e8081141cb
Mikipswso <mickey4649soccer@gmail.com>
2021-12-04 21:23:47 -0500

    updated FoodItem and FoodItems

--- app/src/main/java/nutrition/FoodItem.java
+++ app/src/main/java/nutrition/FoodItem.java
@@ -9,17 +9,20 @@ public class FoodItem{
     String name;
     int amount;
     String[] types;
+    Integer[] nutrition;
 
 
     /**
      * @param name name of the food
      * @param amount amount of the food
      * @param types type of the foo(food category, diet and meal type)
+     * @param nutrition nutritional values of the FoodItem(calories, carbohydrates, fat, protein)
      */
-    public FoodItem(String name, int amount, String[] types) {
+    public FoodItem(String name, int amount, String[] types, Integer[] nutrition) {
         this.name = name;
         this.amount = amount;
         this.types = types;
+        this.nutrition = nutrition;
 
 
 
--- app/src/main/java/nutrition/FoodItems.java
+++ app/src/main/java/nutrition/FoodItems.java
@@ -10,35 +10,48 @@ import java.lang.reflect.Field;
  */
 
 public class FoodItems {
-    final static FoodItem ORANGE = new FoodItem("orange", 50, new String[]{"fruit","snack", "vegan"});
-    final static FoodItem APPLE = new FoodItem("apple", 41, new String[] {"fruit", "snack", "vegan"});
-    final static FoodItem BANANA = new FoodItem("banana", 70, new String[]  {"fruit", "snack", "vegan"});
-    final static FoodItem EGG = new FoodItem("egg", 30, new String[] {"dairy", "breakfast", "vegetarian"});
-    final static FoodItem BROCCOLI = new FoodItem("broccoli", 50, new String[] {"vegetable","lunch","vegan"});
+    final static FoodItem ORANGE = new FoodItem("orange", 50,
+            new String[]{"fruit","snack", "vegan"}, new Integer[]{23, 4, 0, 0});
+    final static FoodItem APPLE = new FoodItem("apple", 182,
+            new String[] {"fruit", "snack", "vegan"}, new Integer[]{95, 25, 0, 0});
+    final static FoodItem BANANA = new FoodItem("banana", 118,
+            new String[]  {"fruit", "snack", "vegan"}, new Integer[]{105, 27, 0, 1});
+    final static FoodItem EGG = new FoodItem("egg", 70,
+            new String[] {"dairy", "breakfast", "vegetarian"}, new Integer[]{70, 0, 5, 6});
+    final static FoodItem BROCCOLI = new FoodItem("broccoli", 100,
+            new String[] {"vegetable","lunch","vegan"}, new Integer[]{34, 7, 0, 3});
     final static FoodItem WHITEBREAD = new FoodItem("white bread",
-            60, new String[] {"grain", "vegetarian", "breakfast"});
-    final static FoodItem CEREAL = new FoodItem("cereal", 30, new String[] {"grain", "breakfast", "vegetarian"});
+            40, new String[] {"grain", "vegetarian", "breakfast"}, new Integer[]{50, 9, 1, 2});
+    final static FoodItem CEREAL = new FoodItem("cereal", 30,
+            new String[] {"grain", "breakfast", "vegetarian"}, new Integer[]{113, 23, 1, 2});
     final static FoodItem CHICKENBREAST = new FoodItem("chicken breast",
-            120, new String[]  {"poultry", "dinner", "none"});
+            85, new String[]  {"poultry", "dinner", "none"}, new Integer[]{110, 2, 3, 20});
     final static FoodItem WHITERICE = new FoodItem("white rice",
-            200, new String[] {"grain", "dinner", "vegan"});
-    final static FoodItem PASTA = new FoodItem("pasta", 180, new String[] {"grain", "dinner", "vegetarian"});
+            45, new String[] {"grain", "dinner", "vegan"}, new Integer[]{160, 38, 0, 3});
+    final static FoodItem PASTA = new FoodItem("pasta", 72,
+            new String[] {"grain", "dinner", "vegetarian"}, new Integer[]{265, 54, 1, 9});
     final static FoodItem ICECREAM = new FoodItem("ice cream",
-            60, new String[] {"dessert", "snack", "vegetarian"});
+            100, new String[] {"dessert", "snack", "vegetarian"}, new Integer[]{250, 25, 10, 4});
     final static FoodItem CHOCOLATE = new FoodItem("chocolate",
-            20, new String[] {"dessert", "snack", "vegetarian"});
+            20, new String[] {"dessert", "snack", "vegetarian"}, new Integer[]{22, 2, 1, 1});
     final static FoodItem FRENCHFRIES = new FoodItem("french fries",
-            90, new String[] {"vegetable", "lunch", "vegan"});
+            84, new String[] {"vegetable", "lunch", "vegan"}, new Integer[]{120, 20, 4, 2});
     final static FoodItem CHICKPEAS = new FoodItem("chickpeas",
-            150, new String[] {"legume", "snack", "vegan"});
+            100, new String[] {"legume", "snack", "vegan"}, new Integer[]{46, 8, 1, 2});
     final static FoodItem OATS = new FoodItem("oats",
-            40, new String[] {"grain", "breakfast", "vegan"});
-    final static FoodItem STEAK = new FoodItem("steak", 130, new String[] {"meat", "dinner", "none"});
-    final static FoodItem TOMATO= new FoodItem("tomato", 50, new String[] {"vegetable", "lunch", "vegan"});
-    final static FoodItem PEANUTS = new FoodItem("peanuts", 30, new String[] {"legume", "snack", "vegan"});
-    final static FoodItem COOKIES = new FoodItem("cookies", 30, new String[] {"dessert", "snack", "vegetarian"});
-    final static FoodItem NOODLES = new FoodItem("noodle", 180, new String[] {"grain", "dinner", "vegetarian"});
-    final static FoodItem LETTUCE = new FoodItem("lettuce", 30, new String[] {"vegetable", "lunch", "vegan"});
+            100, new String[] {"grain", "breakfast", "vegan"}, new Integer[]{371, 59, 8, 12});
+    final static FoodItem STEAK = new FoodItem("steak", 113,
+            new String[] {"meat", "dinner", "none"}, new Integer[]{170, 0, 9, 23});
+    final static FoodItem TOMATO= new FoodItem("tomato", 60,
+            new String[] {"vegetable", "lunch", "vegan"}, new Integer[]{11, 2, 0, 1});
+    final static FoodItem PEANUTS = new FoodItem("peanuts", 30,
+            new String[] {"legume", "snack", "vegan"}, new Integer[]{161, 5, 14, 7});
+    final static FoodItem COOKIES = new FoodItem("cookies", 20,
+            new String[] {"dessert", "snack", "vegetarian"}, new Integer[]{220, 30, 10, 2});
+    final static FoodItem NOODLES = new FoodItem("noodle", 60,
+            new String[] {"grain", "dinner", "vegetarian"}, new Integer[]{200, 41, 1, 7});
+    final static FoodItem LETTUCE = new FoodItem("lettuce", 50,
+            new String[] {"vegetable", "lunch", "vegan"}, new Integer[]{4, 2, 0, 0});
 
 
     /*
