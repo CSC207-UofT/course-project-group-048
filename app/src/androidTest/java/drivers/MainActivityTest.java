4017575405c2b4e123006175de8bde005adeb65e
Mikipswso <mickey4649soccer@gmail.com>
2021-12-07 14:35:16 -0500

    made some tests

--- app/src/androidTest/java/drivers/MainActivityTest.java
+++ app/src/androidTest/java/drivers/MainActivityTest.java
@@ -1,17 +1,45 @@
 package drivers;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import static org.junit.Assert.*;
+
+import android.view.View;
+
+import com.example.loginpage.R;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+import drivers.Activity.MainActivity;
+import drivers.Activity.RegisterActivity;
+
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+@RunWith(JUnit4.class)
 public class MainActivityTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void testLaunch(){
+        onView(withId(R.id.txtViewPassword)).check(matches(withText("Password")));
+    }
+    @Test
+    public void testRegistration() {
+        onView(withId(R.id.buttonCreateAcc)).perform(click());
+        onView(withId(R.id.radioBtnGain)).check(matches(isDisplayed()));
     }
 }
