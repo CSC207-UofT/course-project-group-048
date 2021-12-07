2ac896d0b58cf9dc66f4b0e4fbfa4978c2aa80b5
ramchawla <chawlaram1@gmail.com>
2021-12-07 03:55:10 -0500

    gave birth to the homepage

---
+++ app/src/main/java/drivers/CategoryAdapter.java
@@ -0,0 +1,81 @@
+package drivers;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import drivers.CategoryDomain;
+import com.example.loginpage.R;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    ArrayList<CategoryDomain> categoryDomains;
+
+    public CategoryAdapter(ArrayList<CategoryDomain> categoryDomains) {
+        this.categoryDomains = categoryDomains;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_category, parent, false);
+
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.categoryName.setText(categoryDomains.get(position).getTitle());
+        String picUrl="";
+        switch (position) {
+            case 0: {
+                picUrl = "cat_1";
+                break;
+            }
+            case 1: {
+                picUrl = "cat_2";
+                break;
+            }
+            case 2: {
+                picUrl = "cat_3";
+                break;
+            }
+        }
+
+        int drawableResourceId = holder.itemView.getContext().getResources()
+                .getIdentifier(picUrl, "drawable",
+                        holder.itemView.getContext().getPackageName());
+
+        Glide.with(holder.itemView.getContext())
+                .load(drawableResourceId)
+                .into(holder.categoryPic);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return categoryDomains.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView categoryName;
+        ImageView categoryPic;
+        ConstraintLayout mainLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            categoryName = itemView.findViewById(R.id.categoryName);
+            categoryPic = itemView.findViewById(R.id.categoryPic);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
