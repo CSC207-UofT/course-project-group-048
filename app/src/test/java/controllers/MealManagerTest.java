9b4cb141674dee5845ce40f294d146304c656eb8
willassad <will.assad@mail.utoronto.ca>
2021-12-08 00:19:38 -0500

    add blank images at start

--- app/src/test/java/controllers/MealManagerTest.java
+++ app/src/test/java/controllers/MealManagerTest.java
@@ -14,17 +14,13 @@ public class MealManagerTest {
 
     @Before
     public void setUp() throws Exception {
-        user = new User("John Doe", "johndoe", "0000", "male", 130, 167, 22, "gain");
+        user = new User("John Doe", "johndoe", "0000",
+                "male", 130, 167, 22, "gain");
         mealManager = new MealManager(user);
     }
 
     @Test(timeout = 50)
     public void TestGenerateFoodItems() {
-        System.out.println(mealManager.getTargetCalories());
-        for (Meal meal : mealManager.generateAllMeals()) {
-            for (FoodItem foodItem : meal) {
-                System.out.println(foodItem);
-            }
-        }
+        // add test
     }
 }
