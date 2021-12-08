241c52949f897740d71487cf9ee104240b368c6e
harshshah <harsh108shah@gmail.com>
2021-12-08 13:06:55 -0500

    added lots and lots of detailed javadocs explaining the classes and their methods for the DRIVER classes

--- app/src/main/java/drivers/Adapter/CategoryAdapter.java
+++ app/src/main/java/drivers/Adapter/CategoryAdapter.java
@@ -27,6 +27,13 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         this.categoryDomains = categoryDomains;
     }
 
+    /**
+     * On creation of a new view with parent as its view group, this method instantiates the
+     * corresponding XML file and generating the required new view, returned as a ViewHolder object.
+     * @param parent the parent view group which contains the corresponding category
+     * @param viewType an integer representing the viewType of the view holder
+     * @return the new inflate view object is returned as a view holder object.
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -35,6 +42,14 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         return new ViewHolder(inflate);
     }
 
+    /**
+     * Once the view holder is set using the adapter, this method is called with the view holder
+     * and the required positions of a category, inserting one in its position using
+     * recycler view.
+     * @param holder the view holder that contains information about the categories
+     * @param position the category the position integer is linked with is set and placed using
+     *                 the recycler view
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.categoryName.setText(categoryDomains.get(position).getTitle());
@@ -64,7 +79,10 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
 
     }
 
-
+    /**
+     * A getter method for the size of the categoryDomains attribute.
+     * @return the size of the category domains attribute.
+     */
     @Override
     public int getItemCount() {
         return categoryDomains.size();
@@ -75,6 +93,12 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         ImageView categoryPic;
         ConstraintLayout mainLayout;
 
+        /**
+         * Creates an instance of the ViewHolder object, with its three important attributes
+         * extracted from the itemView parameter.
+         * @param itemView This contains information about the name, picture and layout of each
+         *                 category.
+         */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             categoryName = itemView.findViewById(R.id.categoryName);
--- app/src/main/java/drivers/Domain/CategoryDomain.java
+++ app/src/main/java/drivers/Domain/CategoryDomain.java
@@ -3,10 +3,19 @@ package drivers.Domain;
 public class CategoryDomain {
     private final String title;
 
+    /**
+     * Creates an instance of the CategoryDomain object, used in the CategoryAdapter class
+     * providing information about each category that the adapter processes.
+     * @param title the string representing the information contained in this CategoryDomain object.
+     */
     public CategoryDomain(String title) {
         this.title = title;
     }
 
+    /**
+     * A getter method for the title attribute.
+     * @return the title attribute of the class, which is a string.
+     */
     public String getTitle() {
         return title;
     }
